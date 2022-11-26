c47a9c8b6ff72f2bac4a90aee513c012e6781a55
Ann <niyta@mail.ru>
2022-11-26 21:26:37 +0500

    Initial commit

---
+++ src/main/java/skypro/employeeSpringMockito/EmployeeSpringMockitoApplication.java
@@ -0,0 +1,13 @@
+package skypro.employeeSpringMockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSpringMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSpringMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/employeeSpringMockito/EmployeeSpringMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.employeeSpringMockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSpringMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
